ff4d05f815d838c19aa3d85e70338b841e7c1ec0
Brillianyt <157277894+Brillianyt@users.noreply.github.com>
2024-12-22 16:28:38 +0800

    test_edition

---
+++ srtp_try/git_maven_ast/src/main/java/kaiyang_try/step1/git_maven_ast/App.java
@@ -0,0 +1,32 @@
+package kaiyang_try.step1.git_maven_ast;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jdt.core.*;
+import org.eclipse.jdt.core.dom.*;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.Scanner;
+
+
+public  class App 
+{
+	 public static  void main(String[] args) {
+	       
+
+	       
+
+	    
+   int a=1;
+   int b=2;
+   int c=a+b;
+   System.out.println(c);
+	      
+
+	      
+	        
+	
+	        
+   }
+}
